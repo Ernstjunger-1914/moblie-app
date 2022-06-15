fb5c82c792ce401031c35013d08182ec018534d3
asdf <vot768@naver.com>
2022-06-15 17:12:31 +0900

    1. 프로젝트 생성

---
+++ app/src/main/java/com/ssd/app/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ssd.app.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssd.app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
